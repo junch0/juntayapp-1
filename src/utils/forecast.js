bc24474045cf106c43e20f43e569c071983e189f
Jun Cho <juncho@Juns-MacBook-Pro-2.local>
2020-09-18 15:26:20 -0700

    weather app

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=2e7ec68644774247ab659c8a49695973&query=' + latitude + ',' + longitude + '&units=f' + '&lim=1'
+    
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather server!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location, try another search!', undefined)
+        } else {
+            callback(undefined, response.body.current.weather_descriptions + ' It is currently ' + response.body.current.temperature + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
